9fa92a2ca30bda08e694a65ff46c5b3fbaee7375
heckzi <110593187+heckzi@users.noreply.github.com>
2024-03-12 16:10:12 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,16 @@
+def solution(num):
+    answer = 0
+    if num==1:
+        return 0
+    while(1):
+        if num%2==0:
+            num/=2
+            answer+=1
+        else:
+            num=num*3+1
+            answer+=1
+        if answer==500 and num!=1:
+            return -1
+        if num==1:
+            break
+    return answer
