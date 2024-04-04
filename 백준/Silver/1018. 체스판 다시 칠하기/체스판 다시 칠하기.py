b4febfd0cc0c517019d667a3890d976cc6f1c2fc
heckzi <110593187+heckzi@users.noreply.github.com>
2024-04-05 01:28:22 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 100 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,36 @@
+n,m=map(int,input().split())
+graph=[]
+for i in range(n):
+    graph.append(list(input()))
+
+board1=[['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']
+        ]
+board2=[['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+        ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+        ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']
+        ]
+ans=n*m
+for i in range(0,n-7):
+    for j in range(0,m-7):
+        cnt1=0
+        cnt2=0
+        for a in range(0,8):
+            for b in range(0,8):
+                if board1[a][b]!=graph[i+a][j+b]:
+                    cnt1+=1
+                if board2[a][b]!=graph[i+a][j+b]:
+                    cnt2+=1
+        ans=min(ans,min(cnt1,cnt2))
+print(ans)
